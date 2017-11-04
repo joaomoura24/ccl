32118e25e6736d9cc6181ab7eb23eacbe5281a71
Joao Moura <joaopousa24@gmail.com>
2017-11-04 21:49:22 +0000

    Estimate only the W_A for several windows of the full data set

--- Functions/seperate_data_set/save_estimation_W.m
+++ Functions/seperate_data_set/save_estimation_W.m
@@ -27,7 +27,6 @@ DH = [0.0, 0.31, 0.0, pi/2; % Robot Kinematic model specified by the Denavit-Har
       0.0, 0.21-0.132, 0.0, 0.0];
 robot = SerialLink(DH); % Peters Cork robotics library has to be installed
 Phi_A = def_phia_4_spm(robot); % Phi_A(q): vector of regressors for the Constraint matrix as a function of the configuration
-Phi_b = def_phib_4_spm_sim(robot); % Phi_b(q): vector of regressors for the main task as a function of the configuration
 %--------------------------------------------------------------------------
 %--------------------------------------------------------------------------
 
@@ -45,14 +44,14 @@ gcp(); % Get the current parallel pool
 fprintf(1,'Computing H ...\n');
 x_all = [x{:}]; % all states together as cell
 u_all = [u{:}]; % all actions together as cell
-file_name = 'H.mat';
+file_name = 'H_A.mat';
 if exist(file_name,'file')
     load(file_name);
 else
     H_cell = cell(size(x_all));
     parfor idx=1:length(H_cell)
         % H(q,u): Matrix of regressors as a function of the configuration and action compute number of regressors
-        H_cell{idx} = [feval(Phi_A, x_all{idx})*u_all{idx}; -feval(Phi_b,x_all{idx})];
+        H_cell{idx} = feval(Phi_A, x_all{idx})*u_all{idx};
     end
     H = cell2mat(H_cell);
     save(file_name,'H','H_cell');
@@ -64,18 +63,17 @@ end
 %--------------------------------------------------------------------------
 %% Estimating weights with the all data sets together and saving to file
 fprintf(1, 'Estimating W ...\n');
-getWFileName = @(w,i) strcat('W/w',num2str(w),'i',num2str(i),'.mat');
-[W_hat, W_hat_vec, Hw] = estimateW4windows(H, 3, 100, 1000);
-% tic;
-% ConstraintDim = 3;
-% for window_size = 400:100:2500
-%     for increment = 50:50:200
-%         [W_hat, W_hat_vec, Hw] = estimateW4windows(H, ConstraintDim, increment, window_size);
-%         save(getWFileName(window_size, increment),'W_hat','W_hat_vec','Hw');
-%         clear W_hat W_hat_vec Hw;
-%     end
-% end
-% toc
+getWFileName = @(w,i) strcat('W_A/w',num2str(w),'i',num2str(i),'.mat');
+tic;
+ConstraintDim = 3;
+for window_size = 400:100:2500
+    for increment = 50:50:200
+        [W_hat, W_hat_vec, Hw] = estimateW4windows(H, ConstraintDim, increment, window_size);
+        save(getWFileName(window_size, increment),'W_hat','W_hat_vec','Hw');
+        clear W_hat W_hat_vec Hw;
+    end
+end
+toc
 %--------------------------------------------------------------------------
 %--------------------------------------------------------------------------
 
