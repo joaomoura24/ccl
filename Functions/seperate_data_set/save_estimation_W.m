ab854528281411bf069a73966dae79b6c84e0d78
Joao Moura <joaopousa24@gmail.com>
2017-12-11 12:00:07 +0000

    Delete param vec option

--- Functions/seperate_data_set/save_estimation_W.m
+++ Functions/seperate_data_set/save_estimation_W.m
@@ -9,7 +9,7 @@ addpath(genpath('../'));
 %--------------------------------------------------------------------------
 %--------------------------------------------------------------------------
 fprintf(1,'Getting data ...\n');
-load('data_smooth.mat');
+load('data_test_paper.mat');
 NDem = length(x); % number of demonstrations
 %--------------------------------------------------------------------------
 %--------------------------------------------------------------------------
@@ -27,6 +27,7 @@ DH = [0.0, 0.31, 0.0, pi/2; % Robot Kinematic model specified by the Denavit-Har
       0.0, 0.21-0.132, 0.0, 0.0];
 robot = SerialLink(DH); % Peters Cork robotics library has to be installed
 Phi_A = def_phia_4_spm(robot); % Phi_A(q): vector of regressors for the Constraint matrix as a function of the configuration
+Phi_b = def_phib_4_spm_sim(robot);
 %--------------------------------------------------------------------------
 %--------------------------------------------------------------------------
 
@@ -44,21 +45,22 @@ gcp(); % Get the current parallel pool
 fprintf(1,'Computing H ...\n');
 x_all = [x{:}]; % all states together as cell
 u_all = [u{:}]; % all actions together as cell
-file_name = 'H_A.mat';
-H_fun = @(phi,u) phi*u;
+file_name = 'H_test_paper.mat';
+H_fun = @(phia,phib,u) [phia*u; -phib];
 if exist(file_name,'file')
     load(file_name);
 else
-    H_cell = cell(size(x_all));
+    H_all = cell(size(x_all));
     Phi_A_cell = cell(size(x_all));
-    Phi_A_inv_cell = cell(size(x_all));
-    parfor idx=1:length(H_cell)
+    Phi_b_cell = cell(size(x_all));
+    parfor idx=1:length(H_all)
         % H(phi,u): Matrix of regressors as a function of the configuration and action compute number of regressors
         Phi_A_cell{idx} = feval(Phi_A, x_all{idx});
-        Phi_A_inv_cell{idx} = pinv(Phi_A_cell{idx});
-        H_cell{idx} = feval(H_fun, Phi_A_cell{idx}, u_all{idx});
+        Phi_b_cell{idx} = feval(Phi_b, x_all{idx});
+        H_all{idx} = feval(H_fun, Phi_A_cell{idx},...
+            Phi_b_cell{idx},u_all{idx});
     end
-    save(file_name,'H_cell','x_all','u_all','Phi_A_cell','Phi_A_inv_cell');
+    save(file_name,'H_all','x_all','u_all','Phi_A_cell','Phi_b_cell');
 end
 %--------------------------------------------------------------------------
 %--------------------------------------------------------------------------
@@ -68,17 +70,16 @@ end
 %--------------------------------------------------------------------------
 %--------------------------------------------------------------------------
 fprintf(1, 'Estimating W ...\n');
-getWFileName = @(w,i) strcat('W_A_new/w',num2str(w),'i',num2str(i),'.mat');
+getWFileName = @(w,i) strcat('W_test/w',num2str(w),'i',num2str(i),'.mat');
 tic;
 ConstraintDim = 3;
-for window_size = 1200
-    for increment = 100
-        [W_hat, W_hat_prod, W_hat_vec, Hw, Phi_A_inv_cell_w] = ...
-            estimateW4windows(H_cell, Phi_A_inv_cell, ...
-            ConstraintDim, increment, window_size);
-        save(getWFileName(window_size, increment),'W_hat', 'W_hat_prod',...
-            'W_hat_vec','Hw','Phi_A_inv_cell_w');
-        clear W_hat W_hat_vec W_hat_prod Hw Phi_A_inv_cell_w;
+for window_size = 400
+    for increment = 50
+        [W_hat, W_hat_vec, H_cell] = estimateW4windows(...
+            H_all, ConstraintDim, increment, window_size);
+        save(getWFileName(window_size, increment),...
+            'W_hat','W_hat_vec','H_cell');
+        clear W_hat W_hat_vec H_cell;
     end
 end
 toc
